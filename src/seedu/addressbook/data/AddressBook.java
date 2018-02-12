948fcb33419d90afb8f6e6fb305dde4af58d78b3
slothhy <ohy1994@gmail.com>
2018-02-13 01:12:36 +0800

    Added sort command to sort by alphabetical order

--- src/seedu/addressbook/data/AddressBook.java
+++ src/seedu/addressbook/data/AddressBook.java
@@ -100,6 +100,13 @@ public class AddressBook {
     }
 
     /**
+     * Sort the person list in address book.
+     */
+    public void sortList() {
+        allPersons.sortName();
+    }
+
+    /**
      * Clears all persons and tags from the address book.
      */
     public void clear() {
