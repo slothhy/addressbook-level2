d3ddd6cf429aff7a4dc898968c5966127a673762
slothhy <ohy1994@gmail.com>
2018-02-20 02:47:57 +0800

    Add finding by case-insensitive keywords

--- src/seedu/addressbook/parser/Parser.java
+++ src/seedu/addressbook/parser/Parser.java
@@ -245,7 +245,7 @@ public class Parser {
         }
 
         // keywords delimited by whitespace
-        final String[] keywords = matcher.group("keywords").split("\\s+");
+        final String[] keywords = matcher.group("keywords").toLowerCase().split("\\s+");
         final Set<String> keywordSet = new HashSet<>(Arrays.asList(keywords));
         return new FindCommand(keywordSet);
     }
